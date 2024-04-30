0cea78ac163924b5ac33efb2dac8afbc0f64557a
marcelobesalv <166611596+marcelobesalv@users.noreply.github.com>
2024-04-30 11:49:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ def cria_mapa(n):
     return [[' ']*n for i in range(n)]
 
 def foi_derrotado(m):
-    for i in m: if 'N' in i: return False
+    for i in m: 
+        if 'N' in i: return False
     return True
