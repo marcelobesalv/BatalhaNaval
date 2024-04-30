8a2fb2f246803f357f630670a299d2ee2060cfad
marcelobesalv <166611596+marcelobesalv@users.noreply.github.com>
2024-04-30 18:47:34 -0300

    import config

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from func import *
+import config
 import random
 tmap = int(input('Tamanho do mapa: '))
 mapj = cria_mapa(tmap)
 mapc = mapj
-paiscj = escolher_paises(PAISES)
+paiscj = escolher_paises(config.PAISES)
 paisj = paiscj[1]
 paisc = paiscj[0]
 aloca_navios(mapc,cria_lista_navios(paisc))
