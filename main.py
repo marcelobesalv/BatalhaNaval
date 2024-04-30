587653a67e907d1cc8105f53b1ce1627d0d3544b
marcelobesalv <166611596+marcelobesalv@users.noreply.github.com>
2024-04-30 18:03:34 -0300

    base da estrutura final do jogo

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from func.py import *
+tmap = int(input('Tamanho do mapa: '))
+mapj = cria_mapa(tmap)
+mapc = mapj
+paisj = escolher_paises(PAISES)[1]
+paisc = escolher_paises(PAISES)[0]
+aloca_navios(mapc,cria_lista_navios(paisc))
+aloca_navios_jogador(mapj,cria_lista_navios(paisj))
+while not foi_derrotado(mapc) or not foi_derrotado(mapj):
+  ataque(mapc)
+  ataque_comp(mapj)
+
+
