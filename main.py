6b8d254b5ea8a4d45ab373357dc0a32007cf8dca
marcelobesalv <166611596+marcelobesalv@users.noreply.github.com>
2024-04-30 18:07:44 -0300

    mudança na escolha de pais

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from func.py import *
 tmap = int(input('Tamanho do mapa: '))
 mapj = cria_mapa(tmap)
 mapc = mapj
-paisj = escolher_paises(PAISES)[1]
-paisc = escolher_paises(PAISES)[0]
+paiscj = escolher_paises(PAISES)
+paisj = paiscj[1]
+paisc = paiscj[0]
 aloca_navios(mapc,cria_lista_navios(paisc))
 aloca_navios_jogador(mapj,cria_lista_navios(paisj))
 while not foi_derrotado(mapc) or not foi_derrotado(mapj):
