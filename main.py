59cd4e51cc3186ee210fa3387ce8a7a91cfba7c3
felipesarian <64616194+felipesarian@users.noreply.github.com>
2024-05-01 12:05:44 -0300

    adicionando mensagens

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ mapc = mapj
 paiscj = escolher_paises(config.PAISES)
 paisj = paiscj[1]
 paisc = paiscj[0]
+print(f'\n\nPaíses escolhidos:\nJogador: {paisj}\nComputador: {paisc}\nPosicione seus navios!\n\n')
 aloca_navios(mapc,cria_lista_navios(paisc))
 aloca_navios_jogador(mapj,cria_lista_navios(paisj))
+print('\n\nNavios posicionados, começando jogo!\n\n')
 while not foi_derrotado(mapc) or not foi_derrotado(mapj):
   ataque(mapc)
   ataque_comp(mapj)
